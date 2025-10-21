4b90e34dee8b28e8d72be7c9ff3450243d47d472
Wanly <laggerfeed270@gmail.com>
2025-10-21 16:46:15 +0300

    update

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -124,6 +124,11 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # додай це
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
